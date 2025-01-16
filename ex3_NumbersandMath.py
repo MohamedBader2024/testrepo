1a3230c273c410fd74f5eaddf92c1059238275ea
MohamedBader2024 <165310957+MohamedBader2024@users.noreply.github.com>
2025-01-16 22:35:12 +0200

    Add files via upload

---
+++ ex3_NumbersandMath.py
@@ -0,0 +1,23 @@
+print ("I will now count my chickens:")
+# count the chickens 
+print ("Hens", 25.0 + 30.0 / 6.0)
+print ("Roosters", 100.0 - 25.0 * 3.0 %4.0)
+# Count the eggs 
+print ("Now I will count the eggs:")
+
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# Compare tow quantit
+print ("Is it true that 3 + 2 < 5 - 7?")
+
+print (3 + 2 < 5 - 7)
+
+print ("what is 3 + 2?", 3 + 2)
+print ("waht is 5 - 7?", 5 - 7)
+
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+#More comparesone
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
